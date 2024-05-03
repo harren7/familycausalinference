014eb131fac4bc6d0f026f87d638689a0b87b18f
raichandanisagar <raichandanisagar@gmail.com>
2024-05-03 03:01:05 -0400

    First attempt at putting together a reasonable logistic regression model for a binary outcome of graduation from college

--- code/linear_model.R
+++ code/linear_model.R
@@ -1,24 +1,30 @@
 # load the required libraries
 library(ggplot2)
+library(boot)
+library(pscl)
 
 # load the csv data file
 df <- read.csv("data/preprocessed/inference_base.csv")
+df$college_grad_flag <- ifelse(df$child_edu_level == "Completed college/grad", 1, 0)
+ftrs <- c("child_attend_kindergarten", "povstatus", "child_gender_w1", "parents_edustatus_w1",
+           "hs_avg_score", "mother_fin_security_w2345", "father_in_jail_w1")
 
-# linear regression
-lm_model <- lm(hs_avg_score ~ child_edu_level + mother_mothly_pretax_inc_w1 +
-               parents_edustatus_w1 + mother_fin_security_w2345, data = df)
+target <- c("college_grad_flag")
+model_df <- df[,c(target, ftrs)]
 
-# new target variable 
-# 1: Completed college
-# 0: else
-df$target <- ifelse(df$child_edu_level == "Completed college/grad", 1, 0)
+model_df <- model_df[complete.cases(model_df),]
+print(dim(model_df))
 
-# create df_low_inc
-df_low_inc <- df[df$povstatus == "low-income",]
+logistic_loss <- function(y, yhat){
+  return(mean(-(y*log(yhat) + (1-y)*log(1-yhat))))
+}
 
-# logistic regression
-glm_model <- glm(target ~ child_attend_kindergarten +
-                 parents_edustatus_w1 + mother_fin_security_w2345 + povstatus +
-                 hs_avg_score, data = df, family = binomial)
+set.seed(2020)
+glm_model <- glm(college_grad_flag ~ ., data = model_df, family = binomial)
+null_model <- glm(college_grad_flag ~ 1, data = model_df, family = binomial)
+print(summary(glm_model))
 
-summary(glm_model)
+cv_err <- cv.glm(model_df, glm_model, logistic_loss, K=10)
+gof_measures <- pR2(glm_model)
+print(cv_err$delta[1])
+print(gof_measures)
