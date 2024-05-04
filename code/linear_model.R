5f20957a6811987ca02dd07018a36a5a2646688d
raichandanisagar <raichandanisagar@gmail.com>
2024-05-04 03:36:09 -0400

    Completed predictive lr model

--- code/linear_model.R
+++ code/linear_model.R
@@ -4,47 +4,48 @@ library(boot)
 library(pscl)
 library(glmnet)
 library(caTools)
+library(pROC)
 
 # load the csv data file
 df <- read.csv("data/preprocessed/inference_base.csv")
 df$college_grad_flag <- ifelse(df$child_edu_level == "Completed college/grad", 1, 0)
-cat_ftrs <- c("child_attend_kindergarten","povstatus", "child_gender_w1", 
+cat_ftrs <- c("child_attend_kindergarten","povstatus", "child_gender_w1",
               "parents_edustatus_w1","mother_fin_security_w2345", "father_in_jail_w1")
 num_ftrs <- c("hs_avg_score")
 ftrs <- c(cat_ftrs, num_ftrs)
 target <- c("college_grad_flag")
 
+preprocess <- function(df, cat_ftrs, num_ftrs){
+  for(feature in cat_ftrs){
+    df[[feature]] <- as.factor(df[[feature]])
+  }
+  for(feature in num_ftrs){
+    df[[feature]] <- scale(df[[feature]])
+  }
+  return(df)
+}
+
 model_df <- df[,c(target, ftrs)]
 model_df <- model_df[complete.cases(model_df),]
+
 data_index <- 1:nrow(model_df)
-train_index <- sample(data_index, size = floor(0.8 * length(data_index)))  # 80% for training
+train_index <- sample(data_index, size = floor(0.8 * length(data_index)))
+train_df <- preprocess(model_df[train_index,], cat_ftrs, num_ftrs)
+test_df <- preprocess(model_df[-train_index,], cat_ftrs, num_ftrs)
 
-train_df <- model_df[train_index,]
-test_df <- model_df[-train_index,]
-
-for(feature in cat_ftrs){
-  train_df[[feature]] <- as.factor(train_df[[feature]])
-  test_df[[feature]] <- as.factor(test_df[[feature]])
-}
-
-for(feature in num_ftrs){
-  train_df[[feature]] <- scale(train_df[[feature]])
-  test_df[[feature]] <- scale(test_df[[feature]])
-}
-
-
-X_train <- model.matrix(college_grad_flag ~ . + povstatus:parents_edustatus_w1 + mother_fin_security_w2345:father_in_jail_w1, 
+X_train <- model.matrix(college_grad_flag ~. + povstatus:parents_edustatus_w1 +
+                          mother_fin_security_w2345:father_in_jail_w1 + child_attend_kindergarten:hs_avg_score,
                         data = train_df)
 y_train <- train_df[,target]
-X_test <- model.matrix(college_grad_flag ~ . + povstatus:parents_edustatus_w1 + mother_fin_security_w2345:father_in_jail_w1, 
+X_test <- model.matrix(college_grad_flag ~. + povstatus:parents_edustatus_w1 +
+                         mother_fin_security_w2345:father_in_jail_w1 + child_attend_kindergarten:hs_avg_score,
                        data = test_df)
 y_test <- test_df[,target]
-print(dim(X_train))
-print(dim(X_test))
 
 set.seed(2020)
-lambda_values <- 10^seq(5, -5, length = 100)
-cvfit <- cv.glmnet(X_train, y_train, family = "binomial", lambda = lambda_values, alpha = 0)
+lambda_values <- 10^seq(2, -10, length = 100)
+cvfit <- cv.glmnet(X_train, y_train, family = "binomial", lambda = lambda_values,
+                  alpha = 1, intercept=TRUE)
 plot(cvfit$glmnet.fit, "lambda", label = TRUE)
 plot(cvfit)
 
@@ -57,6 +58,33 @@ calculate_f1 <- function(threshold, probs, actual) {
   f1 <- 2 * precision * recall / (precision + recall)
   return(f1)
 }
+
+probabilities <- predict(cvfit, newx = X_test, s = "lambda.min", type = "response")
+roc_curve <- roc(y_test, probabilities[,1])
+
+optimal_threshold <- coords(roc_curve, x = "best",  best.weights = c(1,0.25),
+                            ret = c("threshold", "precision", "recall"))
+print(optimal_threshold)
+
+final_predictions <- ifelse(probabilities[,1] > optimal_threshold[1,1], 1, 0)
+
+# Compute the confusion matrix
+confusion_matrix <- table(Predicted = final_predictions, Actual = y_test)
+print(confusion_matrix)
+
+thresholds = c(optimal_threshold[1,1])
+f1_score <- sapply(thresholds, function(t) calculate_f1(t, probabilities[,1], y_test))
+print(f1_score)
+
+
+refit_df <- preprocess(model_df, cat_ftrs, num_ftrs)
+X <- model.matrix(college_grad_flag ~. + povstatus:parents_edustatus_w1 +
+                    mother_fin_security_w2345:father_in_jail_w1 + child_attend_kindergarten:hs_avg_score, 
+                  data = refit_df)
+y <- model_df[,target]
+
+final_model <- glmnet(X, y, family="binomial", lambda = cvfit$lambda.min, alpha=1, intercept = TRUE)
+print(coef(final_model))
 
 
 
