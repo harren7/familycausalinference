b93ad6502396ff44f31dd58f55b6734d660c258f
raichandanisagar <raichandanisagar@gmail.com>
2024-05-06 03:17:36 -0400

    Commented linear and causal model code; saved all plots as pngs

--- code/ipw_causal_model.R
+++ code/ipw_causal_model.R
@@ -1,4 +1,4 @@
-# load the required libraries
+############################### PRE-DEFINED FUNCTIONS/SETTINGS ###############################
 library(ggplot2)
 library(boot)
 library(pscl)
@@ -10,12 +10,13 @@ library(halfmoon)
 library(propensity)
 library(dplyr)
 
-# load the csv data file
 df <- read.csv("data/preprocessed/inference_base.csv")
 df$college_grad_flag <- ifelse(df$child_edu_level == "Completed college/grad", 1, 0)
 df$child_attend_kindergarten_flag <- ifelse(df$child_attend_kindergarten == "yes", 1, 0)
 
-cat_ftrs <- c("child_attend_kindergarten","povstatus", "child_gender_w1",
+# df <- df[df$povstatus=="high-income",] ## toggle as necessary
+
+cat_ftrs <- c("child_attend_kindergarten", "child_gender_w1",
               "parents_edustatus_w1","mother_fin_security_w2345", "father_in_jail_w1",
                "cm1ethrace_w1")
 num_ftrs <- c("hs_avg_score", "mother_mothly_pretax_inc_w1")
@@ -36,21 +37,46 @@ preprocess <- function(df, cat_ftrs, num_ftrs){
 model_df <- df[,c(target, treatment, ftrs)]
 model_df <- model_df[complete.cases(model_df),]
 
-propmodel_df <- glm(child_attend_kindergarten_flag ~ mother_mothly_pretax_inc_w1 + parents_edustatus_w1 + cm1ethrace_w1, 
-                        family = "binomial", data = model_df) |>
-  augment(type.predict = "response", data = model_df) |>
+############################### PROPENSITY MODEL ###############################
+
+propmodel <- glm(child_attend_kindergarten_flag ~ mother_mothly_pretax_inc_w1 + parents_edustatus_w1 + 
+                   cm1ethrace_w1, family = "binomial", data = model_df) 
+print(summary(propmodel))
+propmodel_df <- propmodel |> augment(type.predict = "response", data = model_df) |>
   mutate(w_ate = wt_ate(.fitted, child_attend_kindergarten_flag))
 
-propensity_plot <- ggplot(propmodel_df, 
-                          aes(.fitted, fill = factor(child_attend_kindergarten_flag))) +
-  geom_mirror_histogram(bins = 15) +
-  scale_y_continuous(labels = abs) +
-  labs(x = "propensity score", fill = "child_attend_kindergarten_flag")
-print(propensity_plot)
 
 smds <- tidy_smd(propmodel_df, .vars = c(mother_mothly_pretax_inc_w1, parents_edustatus_w1, cm1ethrace_w1), 
                  .group = child_attend_kindergarten_flag, .wts = w_ate)
 print(smds)
+
+propensity_plot <- ggplot(propmodel_df, 
+                          aes(.fitted, group = factor(child_attend_kindergarten))) +
+  geom_mirror_histogram(bins = 15) +
+  geom_mirror_histogram(aes(fill = factor(child_attend_kindergarten), weight=w_ate), 
+                        bins = 15,  alpha=0.5) +
+  scale_y_continuous(labels = abs) +
+  theme(axis.text.y = element_text(angle = 0, vjust = 0.5, hjust=0.5, size=8)) +
+  labs(x = "Propensity score", fill = "Kindergarten?") +
+  theme(axis.text.y = element_text(angle = 0, vjust = 0.5, hjust=0.5, size=8), 
+        axis.text.x = element_text(size=8),
+        plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.10, 0.12), legend.text = element_text(size=9, hjust=0.5), 
+        legend.background = element_rect(fill = "transparent", colour = NA),
+        legend.box.just = "right",
+        strip.text.x = element_text(size = 9),
+        title = element_text(size=11)) +
+  ggtitle("Distribution of propensity scores between treatment groups")
+print(propensity_plot)
+ggsave("data/plots/causal-propensity-plot.png", 
+       dpi=300, width=2000, height=1200, units="px", bg="transparent")
+
+# Description of the propensity plot:
+# In this mirrored histogram, the dark bars represent the unweighted distribution 
+# and the lighter bars represent the distribution weighted by the ATE weights
+
+
+############################### OUTCOME MODEL ###############################
 
 
 outcome_model <- glm(college_grad_flag ~ factor(child_attend_kindergarten_flag), 
@@ -58,3 +84,10 @@ outcome_model <- glm(college_grad_flag ~ factor(child_attend_kindergarten_flag),
                      family = "binomial")
 print(summary(outcome_model))
 
+# Interpretation of outcome model:
+# The coefficient value marks the increase in the log-odds of attending kindergarten 
+# from the base value, i.e. as represented by the intercept. This is the ATE. 
+# After some calculation, we can estimate the ATE for different scenarios as:
+# 1. Low-income: 1.51% -> 5.739% => ATE = 4.229%
+# 2. High-income: 9.43% -> 17.17% => ATE = 7.75%
+# 3. Overall: 5.52% -> 11.923% => ATE = 6.4%
--- code/linear_model.R
+++ code/linear_model.R
@@ -1,19 +1,11 @@
-# load the required libraries
+############################### PRE-DEFINED FUNCTIONS/SETTINGS ###############################
 library(ggplot2)
 library(boot)
 library(pscl)
 library(glmnet)
 library(caTools)
 library(pROC)
-
-# load the csv data file
-df <- read.csv("data/preprocessed/inference_base.csv")
-df$college_grad_flag <- ifelse(df$child_edu_level == "Completed college/grad", 1, 0)
-cat_ftrs <- c("child_attend_kindergarten","povstatus", "child_gender_w1",
-              "parents_edustatus_w1","mother_fin_security_w2345", "father_in_jail_w1")
-num_ftrs <- c("hs_avg_score")
-ftrs <- c(cat_ftrs, num_ftrs)
-target <- c("college_grad_flag")
+library(dplyr)
 
 preprocess <- function(df, cat_ftrs, num_ftrs){
   for(feature in cat_ftrs){
@@ -25,16 +17,49 @@ preprocess <- function(df, cat_ftrs, num_ftrs){
   return(df)
 }
 
+calc_confidence <- function(mean, standard_dev, alpha=0.95, type=2){
+  z_score = qnorm(1-(1-alpha)/type)
+  return (c(mean-z_score*standard_dev, mean+z_score*standard_dev))
+}
+
+calculate_f1 <- function(threshold, probs, actual) {
+  predicted <- ifelse(probs > threshold, 1, 0)
+  confusion_matrix <- table(Predicted = predicted, Actual = actual)
+  precision <- confusion_matrix[2, 2] / sum(confusion_matrix[2, ])
+  recall <- confusion_matrix[2, 2] / sum(confusion_matrix[, 2])
+  f1 <- 2 * precision * recall / (precision + recall)
+  return(f1)
+}
+
+par(mar = c(5, 4, 6.5, 2)) # setting plot margins to be a bit wider
+set.seed(1)
+
+############################### BUILD AND MEASURE GOF OF BASE MODEL ###############################
+
+
+df <- read.csv("data/preprocessed/inference_base.csv")
+df$college_grad_flag <- ifelse(df$child_edu_level == "Completed college/grad", 1, 0)
+
+cat_ftrs <- c("child_attend_kindergarten","povstatus", "child_gender_w1",
+              "parents_edustatus_w1","mother_fin_security_w2345", "father_in_jail_w1")
+num_ftrs <- c("hs_avg_score")
+ftrs <- c(cat_ftrs, num_ftrs)
+target <- c("college_grad_flag")
+
 model_df <- df[,c(target, ftrs)]
 model_df <- model_df[complete.cases(model_df),]
-
 
 base_model <- glm(college_grad_flag ~. + povstatus:parents_edustatus_w1 + 
                     mother_fin_security_w2345:father_in_jail_w1 + child_attend_kindergarten:hs_avg_score,
                   family = "binomial", data = preprocess(model_df, cat_ftrs, num_ftrs))
+base_model_summary <- summary(base_model)
+base_model_coeff <- base_model_summary$coefficients
+
 gof_measures <- pR2(base_model)
-print(summary(base_model))
+print(base_model_summary)
 print(gof_measures)
+
+############################### BUILD AND MEASURE PREDICTIVE LLR ###############################
 
 
 data_index <- 1:nrow(model_df)
@@ -51,26 +76,18 @@ X_test <- model.matrix(college_grad_flag ~. + povstatus:parents_edustatus_w1 +
                        data = test_df)
 y_test <- test_df[,target]
 
-set.seed(2020)
 lambda_values <- 10^seq(2, -10, length = 100)
 cvfit <- cv.glmnet(X_train, y_train, family = "binomial", lambda = lambda_values,
                   alpha = 1, intercept=TRUE)
-plot(cvfit$glmnet.fit, "lambda", label = TRUE)
-plot(cvfit)
 
+plot(cvfit$glmnet.fit, "lambda", label = TRUE, 
+     main = "Feature coefficients vs. regularization (lambda)")
+plot(cvfit, main = "Binomial deviance vs. regularization (lambda)")
 
-calculate_f1 <- function(threshold, probs, actual) {
-  predicted <- ifelse(probs > threshold, 1, 0)
-  confusion_matrix <- table(Predicted = predicted, Actual = actual)
-  precision <- confusion_matrix[2, 2] / sum(confusion_matrix[2, ])
-  recall <- confusion_matrix[2, 2] / sum(confusion_matrix[, 2])
-  f1 <- 2 * precision * recall / (precision + recall)
-  return(f1)
-}
 
 probabilities <- predict(cvfit, newx = X_test, s = "lambda.min", type = "response")
 roc_curve <- roc(y_test, probabilities[,1])
-plot(roc_curve)
+plot(roc_curve, main = "ROC curve")
 
 optimal_threshold <- coords(roc_curve, x = "best",  best.weights = c(1,0.25),
                             ret = c("threshold", "precision", "recall"))
@@ -78,7 +95,6 @@ print(optimal_threshold)
 
 final_predictions <- ifelse(probabilities[,1] > optimal_threshold[1,1], 1, 0)
 
-# Compute the confusion matrix
 confusion_matrix <- table(Predicted = final_predictions, Actual = y_test)
 print(confusion_matrix)
 
@@ -86,6 +102,63 @@ thresholds = c(optimal_threshold[1,1])
 f1_score <- sapply(thresholds, function(t) calculate_f1(t, probabilities[,1], y_test))
 print(f1_score)
 
+############################### CALIBRATION PLOT ###############################
 
+
+calib_data = data.frame(prob = probabilities[,1], 
+                        bin = cut(probabilities[,1], 
+                                  breaks = 10),
+                        class = y_test)
+
+calib_grouped_data = summarise(group_by(calib_data, bin), 
+                               observed = sum(class)/n(), expected = sum(prob)/n(), 
+                               se = sqrt(observed*(1-observed)/n()))
+
+calib_plot = ggplot(calib_grouped_data, aes(expected, observed)) + 
+  geom_point() +
+  geom_errorbar(aes(ymax = observed+1.96*se, ymin = observed-1.96*se)) +
+  geom_abline(intercept = 0, slope = 1, linetype = "solid", color = "red") +
+  xlab("Observed proportion") + ylab("Expected proportion") +
+  scale_x_continuous(minor_breaks = seq(0, 0.8, 0.025), breaks = seq(0, 0.8, 0.1)) + 
+  ggtitle("Calibration plot") + 
+  theme(plot.title = element_text(hjust=0.5))
+
+ggsave("data/plots/llr-calib-plot.png", 
+       dpi=300, width=2000, height=1200, units="px", bg="transparent")
+print(calib_plot)
+
+
+############################### BASE MODEL FEATURE COEFFICIENTS ###############################
+
+sig_ftrs  = c()
+point_est = c()
+ci_lower = c()
+ci_upper = c()
+
+for (i in 1:length(ftrs)+1){
+  if (base_model_coeff[i,"Pr(>|z|)"]>0.05){
+    next
+  }
+  conf = calc_confidence(base_model_coeff[i, "Estimate"], base_model_coeff[i, "Std. Error"])
+  cat("The confidence interval of the feature ", i, " is: [", round(conf[1], 3), ", ", round(conf[2], 3), "]\n", sep='')
+  point_est = c(point_est, base_model_coeff[i, "Estimate"])
+  ci_lower  = c(ci_lower, conf[1])
+  ci_upper = c(ci_upper, conf[2])
+  sig_ftrs = c(sig_ftrs, ftrs[i-1])
+}
+
+coeff_plot = ggplot() +
+  geom_point(aes(x = sig_ftrs, y = point_est)) +
+  geom_errorbar(aes(x = sig_ftrs, ymin = ci_lower, ymax = ci_upper), width = 0.2) +
+  xlab("Variables") + ylab("Estimate") + 
+  labs(title = "Feature coefficients of logistic regression model")+
+  theme(axis.text.y = element_text(angle = 0, vjust = 0.5, hjust=0.5, size=8), 
+        axis.text.x = element_text(angle = 30, size=8, vjust = 0.5),
+        plot.title = element_text(hjust = 0.5),
+        title = element_text(size=11))
+ggsave("data/plots/basemodel-coeff-plot.png", 
+       dpi=300, width=2000, height=1200, units="px", bg="transparent")
+
+print(coeff_plot)
 
 
